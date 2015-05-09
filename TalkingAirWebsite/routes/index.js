6766281592789a2843a7282b119392a51fefaa89
cgarner <chip@garnertotalenergy.com>
2015-05-09 21:59:43 +0200

    Header and footer nav files split out

--- TalkingAirWebsite/routes/index.js
+++ TalkingAirWebsite/routes/index.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index');
+    res.render('index', { pageName: 'Home' });
 });
 
 router.get('/about', function (req, res) {
-    res.render('about');
+    res.render('about', { pageName: 'About' });
 });
 
 router.get('/how', function (req, res) {
-    res.render('how');
+    res.render('how', { pageName: 'How' });
 });
 
 router.get('/contact', function (req, res) {
-    res.render('contact');
+    res.render('contact', { pageName: 'Contact' });
 });
 
 router.get('/what', function (req, res) {
-    res.render('what');
+    res.render('what', { pageName: 'What' });
 });
 
 router.get('/why', function (req, res) {
-    res.render('why');
+    res.render('why', { pageName: 'Why' });
 });
 
 module.exports = router;
